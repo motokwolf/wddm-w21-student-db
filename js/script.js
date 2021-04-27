787facaf721280750fcc90dc3bb2c0f623f37bbf
Rocco P <roccopan@gmail.com>
2021-04-27 11:46:40 -0400

    quick filter

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Some data
 const studentsAr = [
   {
     id: 234,
@@ -20,25 +21,63 @@ const studentsAr = [
   }
 ]
 
+
 // Select the element (a <ul> in this case) that will hold all of the student records
 const studentTable = document.querySelector(`#studentTable`)
 
+const setStudentToTable = function(theArray) {
 
-// For each of the students in our "database"
-studentsAr.forEach((student) => {
+  // Clear out information that already exists
+  studentTable.innerHTML = ``
 
-  // Create a list item (<li>) that is not yet added to the document
-  const listItem = document.createElement(`li`)
+  // For each of the students in our "database"
+  theArray.forEach((student) => {
 
-  // Modify this new element, just like we would any other DOM element
-  listItem.classList.add(`student`)
-  listItem.innerHTML = `
-    <img src="img/${student.photo}" alt="${student.name.first}" class="photo">
-    <b>${student.name.last}, ${student.name.first}</b>
-    <data value="${student.gpa }">${ student.gpa } GPA</data>
-  `
+    // Create a list item (<li>) that is not yet added to the document
+    const listItem = document.createElement(`li`)
 
-  // Add the new <li> element to the document
-  studentTable.appendChild(listItem)
+    // Modify this new element, just like we would any other DOM element
+    listItem.classList.add(`student`)
+    listItem.innerHTML = `
+      <img src="img/${student.photo}" alt="${student.name.first}" class="photo">
+      <b>${student.name.last}, ${student.name.first}</b>
+      <data value="${student.gpa }">${ student.gpa } GPA</data>
+    `
+
+    // Add the new <li> element to the document
+    studentTable.appendChild(listItem)
+  })
+
+}
+
+
+setStudentToTable(studentsAr)
+
+
+
+
+
+
+const filterGpa = document.querySelector(`#filterGpa`)
+
+filterGpa.addEventListener(`input`, function(event) {
+  const theRange = event.target
+  const theOutput = document.querySelector(`output[for="filterGpa"]`)
+
+  theOutput.textContent = theRange.value
+
+
+  // filter() is a loop!!!
+  const filteredStudent = studentsAr.filter(function(student) {
+    if (student.gpa >= Number(theRange.value)) {
+      return true
+    } else {
+      return false
+    }
+    //return student.gpa > 3
   
+  })
+
+  setStudentToTable(filteredStudent)
+
 })
